de312d88b53e2de8c0a3eb3432aa52b3f5363ef0
lyanhminh11 <lyanhminh11@gmail.com>
2026-04-09 07:36:57 -0500

    fix variable name

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,5 +1,5 @@
 resource "terraform_data" "replacement" {
   provisioner "local-exec" {
-    command = "echo 'This is the ECS ${var.env}' environment saying, '${var.text}'"
+    command = "echo 'This is the ECS ${var.environment}' environment saying, '${var.text}'"
   }
 }
