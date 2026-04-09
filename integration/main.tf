5c9020db080bb6b58731e2d85167f7bbe3ceb79f
lyanhminh11 <lyanhminh11@gmail.com>
2026-04-09 07:52:00 -0500

    fix integration script path

--- integration/main.tf
+++ integration/main.tf
@@ -2,5 +2,5 @@ module "service" {
   source = "github.com/lyanhminh/test-actions//modules/eks?ref=0.0.1"
 
   environment = "integration"
-  text        = "mary"
+  text        = var.text
 }
--- qa/main.tf
+++ qa/main.tf
@@ -2,5 +2,5 @@ module "service" {
   source = "github.com/lyanhminh/test-actions//modules/eks?ref=0.0.1"
 
   environment = "qa"
-  text        = "jerry"
+  text        = var.text
 } 
