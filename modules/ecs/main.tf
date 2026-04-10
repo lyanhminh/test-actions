e0f152dd79936b4e030c25ab03291ece9c939497
Minh Ly <minh.ly@bluesight.com>
2026-04-10 11:31:34 -0500

    fix quotes

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,5 +1,5 @@
 resource "terraform_data" "replacement" {
   provisioner "local-exec" {
-    command = "echo 'This is the ECS ${var.environment}' environment saying, '${var.text}' running with build tag ${var.build_tag}'"
+    command = "echo 'This is the ECS ${var.environment}' environment saying, '${var.text}' running with build tag '${var.build_tag}'"
   }
 }
--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,5 +1,5 @@
 resource "terraform_data" "replacement" {
   provisioner "local-exec" {
-    command = "echo 'This is the EKS ${var.environment}' environment saying '${var.text}'"
+    command = "echo 'This is the EKS ${var.environment}' environment saying '${var.text}' on build tag '${var.build_tag}'" "
   }
 }
